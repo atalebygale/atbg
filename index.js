036af4c3a6440a9886430c85954b1c7382051cc0
Laura Campbell <lauracampbell@Lauras-MacBook-Air.local>
2022-04-01 09:37:31 -0400

    made header resizable

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// Cat to human yeasr
+const catToHumanYears = (catYears) => {
+  if (catYears < 1) {
+      return catYears * 15
+  } else if (catYears < 2) {
+      return 15 + ((catYears - 1) * 9);
+  }
+  const firstYear = 15
+  const secondYear = 9
+  const laterYears = (catYears - 2) * 4
+  const humanYears = firstYear + secondYear + laterYears
+  return humanYears
+}
+ const printCatToHumanYears = () => {
+ const catAge = document.getElementById("catage").value;
+ const catAgeNumber = parseInt(catAge);
+ return `A ${catAge} year old cat is ${catToHumanYears(catAgeNumber)} in human years.`
+}
+
+window.onload = () => {
+  //
+ let button = document.querySelector("#btn");
+ button.addEventListener("click", ()=> {
+     console.log("clicked");
+     const result = printCatToHumanYears();
+     const answerBox = document.querySelector("#result");
+     answerBox.innerHTML = result;
+     window.scrollTo(0, document.body.scrollHeight);
+ })
+
+
+};
+
+
+// Clock
 setInterval(setClock, 1000)
 
 const hourHand = document.querySelector('[data-hour-hand')
@@ -22,3 +57,11 @@ setClock()
 
 // Nursing organizer
 
+// PI javascript
+
+const piButton = document.getElementById('piButton');
+
+piButton.addEventListener('click', function handleClick() {
+  piButton.innerHTML = '3.14159265359'});
+
+piButton(handleClick());
