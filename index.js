03ee936c3f9de751847950cf0407e31ad6bfa23d
Laura Campbell <lauracampbell@Lauras-MacBook-Air.local>
2022-04-15 18:23:09 -0400

    added weddding countdown

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ window.onload = () => {
      window.scrollBy(0, answerBox.scrollHeight + 50);
   
  })
+ setInterval(writeRemTime, 1000);
 };
 
 
@@ -86,5 +87,44 @@ const celsiusToFahrenheit = () => {
 const tempButton = document.getElementById("tempButton");
 tempButton.addEventListener("click", celsiusToFahrenheit);
 
-// UNC Duke Poll
+// Wedding Countdown
 
+const countdown = () => {
+  let now = new Date();
+  let eventDate = new Date (2022, 4, 1);
+
+  let currentTime = now.getTime();
+  let eventTime = eventDate.getTime();
+
+  let remTime = eventTime - currentTime;
+
+  let s = Math.floor(remTime / 1000); //total seconds remaining
+  let m = Math.floor(s / 60); //total minutes
+  let h = Math.floor(m / 60);//total hours
+  let d = Math.floor(h / 24); //total days
+
+  let remDays = d
+  let remHours = h % 24;
+  let remMinutes = m % 60;
+  let remSeconds = s % 60;
+
+  return {
+    remDays,
+    remHours,
+    remMinutes,
+    remSeconds
+  }
+}
+
+const writeRemTime = () => {
+  const dayContainer = document.getElementById("days");
+  const hourContainer = document.getElementById("hours");
+  const minuteContainer = document.getElementById("minutes");
+  const secondContainer = document.getElementById("seconds");
+
+  const remTime = countdown();
+  dayContainer.innerHTML = remTime.remDays;
+  hourContainer.innerHTML = remTime.remHours;
+  minuteContainer.innerHTML = remTime.remMinutes;
+  secondContainer.innerHTML = remTime.remSeconds;
+}
